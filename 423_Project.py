848842ea57ce3fbd92e5cc0f0c7c1e2e036fd13d
ADIB <adibahmed009@gmail.com>
2025-05-09 23:59:53 +0600

    Add files via upload

---
+++ 423_Project.py
@@ -0,0 +1,547 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+import math
+import time
+import random
+
+
+def create_asteroid():
+    x = random.randint(-10000, 10000)
+    y = random.randint(-10000, 10000)
+    z = random.randint(-10000, 10000)
+    return (x, y, z)
+
+
+def create_enemy_spaceship():
+    x = random.randint(-10000, 10000)
+    y = random.randint(-10000, 10000)
+    return (x, y)
+
+
+player_pos = (0, 0, 0, 0)
+x, y, z, angle = player_pos
+camera_pos = (x, y+70, z+100)
+t1 = int(time.time())
+t2 = int(time.time())
+fovY = 120
+player_life = 100
+score = 0
+fpp = False
+game_state = True
+enemy_spaceship = []
+asteroid = []
+bullet = []
+ang = 0
+map_edge = False
+ship_color = (0.95, 0.95, 0.86)
+ship_speed = 1
+d, h = -500, 500
+
+
+for i in range(100):
+    asteroid.append(create_asteroid())
+
+for i in range(20):
+    enemy_spaceship.append(create_enemy_spaceship())
+
+
+def spawn_enemy_spaceship():
+    global enemy_spaceship, player_pos
+    x, y, z, ang = player_pos
+    for i in enemy_spaceship:
+        x_e, y_e = i
+        draw_enemy_spaceship(x_e, y_e, z)
+
+
+def draw_text(x, y, text, color, flag=False, font=GLUT_BITMAP_HELVETICA_18):
+    global game_state, player_life
+
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+
+    # Set up an orthographic projection that matches window coordinates
+    gluOrtho2D(0, 1000, 0, 800)  # left, right, bottom, top
+
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+
+    if flag:
+        # health bar
+        glPointSize(40)
+        glLineWidth(10)
+        glBegin(GL_LINES)
+        glColor3f(1, 0, 0)
+        glVertex2f(165, 775)
+        glVertex2f(165 + player_life * 5, 775)
+        glEnd()
+        glLineWidth(1)
+
+    r, g, b = color
+    glColor3f(r, g, b)
+    # Draw text at (x, y) in screen coordinates
+    glRasterPos2f(x, y)
+    for ch in text:
+        glutBitmapCharacter(font, ord(ch))
+
+    # Restore original projection and modelview matrices
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+
+
+def draw_bullet():
+    global bullet
+    glPushMatrix()
+
+    for b in bullet:
+        x, y, z, dir = b
+
+        glColor3f(1, 0, 0)
+        glTranslate(x, y, z)
+        glutSolidCube(20)
+        glTranslate(-x, -y, -z)
+
+    glPopMatrix()
+
+
+def keyboardListener(key, x, y):
+    global player_pos, player_life, score, fpp, game_state, enemy_spaceship, asteroid, map_edge, camera_pos, bullet, ang, ship_color, ship_speed, t1, d, h
+
+    if key == b'w' and game_state:
+        t = int(time.time())
+        if (t - t1) > 10:
+            t1 = t
+            ship_speed = 2
+
+    if key == b's' and game_state:
+        pass
+
+    if key == b'a' and game_state:
+        x1, y1, z1, angle = player_pos
+        angle += 1
+        player_pos = x1, y1, z1, angle
+
+    if key == b'd' and game_state:
+        x1, y1, z1, angle = player_pos
+        angle -= 1
+        player_pos = x1, y1, z1, angle
+
+    if key == b'r':
+        player_pos = (0, 0, 0, 0)
+        x, y, z, angle = player_pos
+        camera_pos = (x, y+70, z+100)
+        t1 = int(time.time())
+        t2 = int(time.time())
+        fovY = 120
+        player_life = 100
+        score = 0
+        fpp = False
+        game_state = True
+        enemy_spaceship = []
+        asteroid = []
+        bullet = []
+        ang = 0
+        map_edge = False
+        ship_color = (0.95, 0.95, 0.86)
+        ship_speed = 1
+        d, h = -500, 500
+        for i in range(100):
+            asteroid.append(create_asteroid())
+
+        for i in range(20):
+            enemy_spaceship.append(create_enemy_spaceship())
+
+
+def specialKeyListener(key, x, y):
+    global camera_pos, fpp, game_state, player_pos
+    x, y, z, angle = player_pos
+
+    if game_state:
+        if key == GLUT_KEY_UP:
+            z += 50
+
+        if key == GLUT_KEY_DOWN:
+            z -= 50
+
+        player_pos = (x, y, z, angle)
+        camera_pos = x, y + 70, z + 100
+        if fpp:
+            camera_pos = x, y + 500, z + 530
+
+
+def mouseListener(button, state, x, y):
+    """
+    Handles mouse inputs for firing bullets (left click) and toggling camera mode (right click).
+    """
+
+    global fpp, camera_pos, player_pos, bullet, d, h
+    if button == GLUT_LEFT_BUTTON and state == GLUT_DOWN:
+        x1, y1, z1, angle = player_pos
+        bullet.append((x1, y1, z1, angle))
+
+    if button == GLUT_RIGHT_BUTTON and state == GLUT_DOWN:
+        if fpp:
+            d, h = -500, 500
+        else:
+            d, h = -100, 100
+        fpp = not fpp
+
+def shoot():
+    global t2, enemy_spaceship,enemy_bullet,player_pos
+    for i in enemy_spaceship:
+        x,y = i
+        t = int(time.time())
+        if (t - t2) > 3:
+            t2 = t
+            enemy_bullet.append((x,y,player_pos[2],player_pos[3]))
+def setupCamera():
+    """
+    Configures the camera's projection and view settings.
+    Uses a perspective projection and positions the camera to look at the target.
+    """
+    global fpp, player_pos, camera_pos, d, h
+
+    glMatrixMode(GL_PROJECTION)  # Switch to projection matrix mode
+    glLoadIdentity()  # Reset the projection matrix
+    # Set up a perspective projection (field of view, aspect ratio, near clip, far clip)
+    gluPerspective(fovY, 1.25, 0.1, 20000)  # Think why aspect ration is 1.25?
+    glMatrixMode(GL_MODELVIEW)  # Switch to model-view matrix mode
+    glLoadIdentity()  # Reset the model-view matrix
+
+    # Extract camera position and look-at target
+    x1, y1, z1, angle = player_pos
+    x2, y2, z2 = camera_pos
+
+    rad = math.radians(angle)
+
+    dx = d * math.sin(rad)
+    dy = -d * math.cos(rad)
+
+    cx = x1 + dx
+    cy = y1 + dy
+    cz = z1 + h
+
+    gluLookAt(cx, cy, cz,  # Camera position
+              x1, y1, z1,  # Camera position
+              0, 0, 1)  # Set the camera to look at the target
+
+
+def idle():
+    """
+    Idle function that runs continuously:
+    - Triggers screen redraw for real-time updates.
+    """
+    # Ensure the screen updates with the latest changes
+    global bullet, enemy_spaceship, player_pos, game_state, ship_speed
+
+    if game_state:
+        x1, y1, z1, angle = player_pos
+        dx = ship_speed * math.sin(math.radians(angle))
+        dy = ship_speed * math.cos(math.radians(angle))
+
+        player_pos = x1 + dx, y1 - dy, z1, angle
+
+        for i in range(len(bullet)):
+            x, y, z, dir = bullet[i]
+            y -= 7 * math.cos(math.radians(dir))
+            x += 7 * math.sin(math.radians(dir))
+            bullet[i] = x, y, z, dir
+
+        x1, y1, z1, angle = player_pos
+
+        for j in range(len(enemy_spaceship)):
+            x2, y2 = enemy_spaceship[j]
+            x = x1 - x2
+            y = y1 - y2
+            d = math.degrees(math.atan2(y, x))
+            if d < 0:
+                d += 360
+
+            x2 += 1 * math.cos(math.radians(d))
+            y2 += 1 * math.sin(math.radians(d))
+            enemy_spaceship[j] = (x2, y2)
+
+    glutPostRedisplay()
+
+
+def check_range():
+    global player_pos, game_state
+    x, y, z, angle = player_pos
+    if (x > 10000) or (x < -10000) or (y > 10000) or (y < -10000) or (z > 10000) or (z < -10000):
+        game_state = False
+    # temp = []
+    # for i in range(len(bullet)):
+    #     x, y, z, dir = bullet[i]
+    #     if (x >= -600) and (x <= 600) and (y >= 0) and (y <= 600):
+    #         temp.append((x, y, z, dir))
+    # bullet = temp
+
+
+def check_hit():
+    global enemy_spaceship, bullet, score, player_pos, player_life,asteroid,ship_color,enemy_bullet
+
+    x3, y3, z3, angle = player_pos
+
+    for i in range(len(bullet)):
+        x1, y1, z1, dir = bullet[i]
+        for j in range(len(enemy_spaceship)):
+            x2, y2 = enemy_spaceship[j]
+            d1 = ((x2 - x1)**2 + (y2 - y1)**2)**(0.5)
+            if d1 < 25:
+                score += 1
+                bullet[i] = (-100000, -1000000, -100000 ,dir)
+                enemy_spaceship[j] = create_enemy_spaceship()
+
+    temp = []
+    for i in range(len(bullet)):
+        x, y, z, dir = bullet[i]
+        if (z != -100000):
+            temp.append((x, y, z, dir))
+    bullet = temp
+
+    for k in range(len(enemy_spaceship)):
+        x2, y2 = enemy_spaceship[k]
+        d2 = ((x2 - x3)**2 + (y2 - y3)**2)**(0.5)
+        if d2 < 50:
+            player_life -= 10
+            r, g, b = ship_color
+            r += 0.1
+            g -= 0.1
+            b -= 0.1
+            ship_color = (r, g, b)
+            enemy_spaceship[k] = create_enemy_spaceship()
+    for a in range(len(asteroid)):
+        x_a, y_a, z_a = asteroid[a]
+        d = ((x_a - x3) ** 2 + (y_a - y3) ** 2 + (z_a - z3) ** 2) ** (0.5)
+        if d < 150:
+            player_life -= 5
+            r, g, b = ship_color
+            r += 0.1
+            g -= 0.1
+            b -= 0.1
+            ship_color = (r, g, b)
+            asteroid[a] = create_asteroid()
+
+    for i in range(len(enemy_bullet)):
+        x,y,z,d = enemy_bullet[i]
+        dr = ((x - x3) ** 2 +(y - y3)**2 + (z - z3) ** 2) **(0.5)
+        if dr < 50:
+            enemy_bullet[i] = (-1000000, -1000000, -100000, d)
+            player_life -= 0.5
+            r, g, b = ship_color
+            r += 0.01
+            g -= 0.01
+            b -= 0.01
+            ship_color = (r, g, b)
+    t = []
+    for i in range(len(enemy_bullet)):
+        x, y, z, dir = enemy_bullet[i]
+        if (z != -100000):
+            t.append((x, y, z, dir))
+    enemy_bullet = t
+
+
+def game_info():
+    global game_state, player_life, score, map_edge, t1
+
+    if game_state:
+        draw_text(10, 770, f"Spaceship Health: ", (1, 1, 1), True)
+        draw_text(10, 740, f"Game Score: {score}", (1, 1, 1))
+        t = int(time.time())
+        if ((t - t1) > 10 and t % 2 == 0):
+            draw_text(
+                300, 680, "Speed Boost is available now! Press W to activate", (1, 1, 0.3))
+
+        if map_edge:
+            if (t % 2 == 0):
+                draw_text(
+                    10, 710, "Approaching the firewall !!\nTurn back immediately!!", (1, 0.3, 0))
+    else:
+        draw_text(10, 770, f"Game is over. Your Score is {score}", (1, 1, 1))
+        draw_text(10, 740, f'Press "R" to RESTART the Game.', (1, 1, 1))
+
+
+def draw_game_space():
+    global player_pos, map_edge
+    x, y, z, angle = player_pos
+
+    glBegin(GL_QUADS)
+
+    if x < -8000:
+        glColor3d(1, 0.92, 0)
+        glVertex3f(-10000, -10000, -10000)
+        glVertex3f(-10000, -10000, 10000)
+        glVertex3f(-10000, 10000, 10000)
+        glVertex3f(-10000, 10000, -10000)
+
+    if x > 8000:
+        glColor3d(1, 0.92, 0)
+        glVertex3f(10000, -10000, -10000)
+        glVertex3f(10000, -10000, 10000)
+        glVertex3f(10000, 10000, 10000)
+        glVertex3f(10000, 10000, -10000)
+
+    if y < -8000:
+        glColor3d(1, 0.92, 0)
+        glVertex3f(10000, -10000, -10000)
+        glVertex3f(10000, -10000, 10000)
+        glVertex3f(-10000, -10000, 10000)
+        glVertex3f(-10000, -10000, -10000)
+
+    if y > 8000:
+        glColor3d(1, 0.92, 0)
+        glVertex3f(10000, 10000, -10000)
+        glVertex3f(10000, 10000, 10000)
+        glVertex3f(-10000, 10000, 10000)
+        glVertex3f(-10000, 10000, -10000)
+
+    if z < -8000:
+        glColor3d(1, 0.92, 0)
+        glVertex3f(10000, 10000, -10000)
+        glVertex3f(10000, -10000, -10000)
+        glVertex3f(-10000, -10000, -10000)
+        glVertex3f(-10000, 10000, -10000)
+
+    if z > 8000:
+        glColor3d(1, 0.92, 0)
+        glVertex3f(10000, 10000, 10000)
+        glVertex3f(10000, -10000, 10000)
+        glVertex3f(-10000, -10000, 10000)
+        glVertex3f(-10000, 10000, 10000)
+
+    glEnd()
+
+
+def draw_spaceship():
+    global player_pos, ship_color
+    x, y, z, ang = player_pos
+    r, g, b = ship_color
+
+    glPushMatrix()
+    glColor3f(r, g, b)
+    glTranslatef(x, y, z)
+    glRotate(ang, 0, 0, 1)
+    glutSolidCube(90)
+    glColor3f(1, 0.647, 0)
+    glTranslatef(30, 30, 0)
+    glRotate(90, 0, 0, 1)
+    glRotate(90, 0, 1, 0)
+    gluCylinder(gluNewQuadric(), 20, 2, 50, 10, 10)
+    glTranslatef(0, 60, 0)
+    gluCylinder(gluNewQuadric(), 20, 2, 50, 10, 10)
+    glColor3f(0.55, 0.57, 0.55)
+    glRotate(90, 0, 1, 0)
+    glRotate(90, 0, 1, 0)
+    glTranslatef(0, -30, 100)
+    gluCylinder(gluNewQuadric(), 20, 2, 180, 10, 10)
+    glPopMatrix()
+
+
+def draw_asteroid():
+    global asteroid
+    glPushMatrix()
+    for i in asteroid:
+        x, y, z = i
+        glColor3f(0.75, 0.75, 0.75)
+        glTranslatef(x, y, z)
+        gluSphere(gluNewQuadric(), 150, 10, 10)
+        glTranslatef(-x, -y, -100)
+    glPopMatrix()
+
+
+def draw_enemy_spaceship(x, y, z):
+    global player_pos
+    x1, y1, z1, ang = player_pos
+    glPushMatrix()
+    glColor3f(0.529, 0.81, 0.921)
+    glTranslatef(x, y, z)
+    glRotate(ang, 0, 0, 1)
+    glutSolidCube(90)
+    glColor3f(1, 0.647, 0)
+    glTranslatef(30, 30, 0)
+    glRotate(90, 0, 0, 1)
+    glRotate(90, 0, 1, 0)
+    gluCylinder(gluNewQuadric(), 20, 2, 50, 10, 10)
+    glTranslatef(0, 60, 0)
+    gluCylinder(gluNewQuadric(), 20, 2, 50, 10, 10)
+    glColor3f(0.55, 0.57, 0.55)
+    glRotate(90, 0, 1, 0)
+    glRotate(90, 0, 1, 0)
+    glTranslatef(0, -30, 100)
+    gluCylinder(gluNewQuadric(), 20, 2, 180, 10, 10)
+    glPopMatrix()
+
+
+def run_game():
+    global game_over, game_state, player_pos, map_edge, ship_speed, player_life
+    x, y, z, angle = player_pos
+    if player_life <= 0:
+        game_state = False
+
+    if game_state:
+        draw_asteroid()
+        spawn_enemy_spaceship()
+        draw_spaceship()
+        draw_bullet()
+        draw_game_space()
+        check_hit()
+        check_range()
+
+        if (x > 8000) or (x < -8000) or (y > 8000) or (y < -8000) or (z > 8000) or (z < -8000):
+            map_edge = True
+        else:
+            map_edge = False
+
+        t = int(time.time())
+        if (t - t1) > 3:
+            ship_speed = 1
+    game_info()
+
+
+def showScreen():
+    """
+    Display function to render the game scene:
+    - Clears the screen and sets up the camera.
+    - Draws everything of the screen
+    """
+    # Clear color and depth buffers
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()  # Reset modelview matrix
+    glViewport(0, 0, 1000, 800)  # Set viewport size
+
+    setupCamera()  # Configure camera perspective
+
+    run_game()
+
+    # draw_shapes()
+
+    # Swap buffers for smooth rendering (double buffering)
+    glutSwapBuffers()
+
+
+# Main function to set up OpenGL window and loop
+def main():
+    glutInit()
+    # Double buffering, RGB color, depth test
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(1000, 800)  # Window size
+    glutInitWindowPosition(0, 0)  # Window position
+    wind = glutCreateWindow(b"Bullet Frenzy")  # Create the window
+
+    glutDisplayFunc(showScreen)  # Register display function
+    glutKeyboardFunc(keyboardListener)  # Register keyboard listener
+    glutSpecialFunc(specialKeyListener)
+    glutMouseFunc(mouseListener)
+    # Register the idle function to move the bullet automatically
+    glutIdleFunc(idle)
+
+    glutMainLoop()  # Enter the GLUT main loop
+
+
+if __name__ == "__main__":
+    main()
